0d20a98c5f2b147bb8b004844e1f3ec9a0a36751
Yanis-Ourir <ourir.yanis@gmail.com>
2023-01-27 16:34:04 +0100

    troisième push

--- assets/maquette/lecteur_audio.sql
+++ assets/maquette/lecteur_audio.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost
--- Généré le : mar. 24 jan. 2023 à 09:56
+-- Généré le : ven. 27 jan. 2023 à 16:32
 -- Version du serveur : 10.4.27-MariaDB
 -- Version de PHP : 8.2.0
 
@@ -24,6 +24,29 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Structure de la table `comments`
+--
+
+CREATE TABLE `comments` (
+  `id` int(11) NOT NULL,
+  `commentaire` varchar(255) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `id_music` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `comments`
+--
+
+INSERT INTO `comments` (`id`, `commentaire`, `id_user`, `id_music`) VALUES
+(1, 'One piece > DBZ', 1, 2),
+(2, 'Force à lui', 2, 3),
+(3, 'Le mec au dessus a raison', 3, 2),
+(4, 'CR7 IS THE BEST PLAYER IN HISTORY', 4, 2);
+
+-- --------------------------------------------------------
+
+--
 -- Structure de la table `musics`
 --
 
@@ -32,8 +55,19 @@ CREATE TABLE `musics` (
   `musicName` varchar(255) NOT NULL,
   `musicLink` varchar(255) NOT NULL,
   `Artist` varchar(255) NOT NULL,
-  `musicTime` time NOT NULL
+  `musicImage` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `musics`
+--
+
+INSERT INTO `musics` (`id`, `musicName`, `musicLink`, `Artist`, `musicImage`) VALUES
+(1, 'STOP TRYING TO BE GOD', '../assets/musics/stop-trying-to-be-god.mp3', 'Travis Scott', '../assets/musics/astroworld.jpg'),
+(2, 'We ARE', '../assets/musics/onepiece-audio.mp3', 'One Piece', '../assets/musics/luffy.png'),
+(3, 'Sundance', '../assets/musics/Nepal - Sundance.mp3', 'Népal', '../assets/musics/nepal.jpg'),
+(4, 'Une main lave l\'autre', '../assets/musics/umla.mp3', 'Alpha Wann', '../assets/musics/umla.jpg'),
+(5, 'CP_009_ Évidemment', '../assets/musics/evidemment.mp3', 'Angèle Ft Jsaispas', '../assets/musics/nonante-cinq.jpg');
 
 -- --------------------------------------------------------
 
@@ -43,13 +77,30 @@ CREATE TABLE `musics` (
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
-  `pseudo` varchar(20) NOT NULL,
-  `comments` varchar(255) NOT NULL
+  `pseudo` varchar(20) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Déchargement des données de la table `user`
+--
+
+INSERT INTO `user` (`id`, `pseudo`) VALUES
+(1, 'Yanis'),
+(2, 'FanDeNépal'),
+(3, 'Ahmed'),
+(4, 'SERHATHINIOH');
 
 --
 -- Index pour les tables déchargées
 --
+
+--
+-- Index pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `id_user` (`id_user`),
+  ADD KEY `id_music` (`id_music`);
 
 --
 -- Index pour la table `musics`
@@ -68,16 +119,33 @@ ALTER TABLE `user`
 --
 
 --
+-- AUTO_INCREMENT pour la table `comments`
+--
+ALTER TABLE `comments`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
 -- AUTO_INCREMENT pour la table `musics`
 --
 ALTER TABLE `musics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT pour la table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Contraintes pour les tables déchargées
+--
+
+--
+-- Contraintes pour la table `comments`
+--
+ALTER TABLE `comments`
+  ADD CONSTRAINT `comments_ibfk_1` FOREIGN KEY (`id_user`) REFERENCES `user` (`id`),
+  ADD CONSTRAINT `comments_ibfk_2` FOREIGN KEY (`id_music`) REFERENCES `musics` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
